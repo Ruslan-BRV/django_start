2dc6f597018a9d94e8bc482b2c998a68ff42e852
Ruslan_BRV <ruslan10897@mail.ru>
2024-03-06 15:00:07 +0300

    до настройки урлов

--- cards/views.py
+++ cards/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
 
+
 def main(request):
     return HttpResponse("Привет мир!", status=200)
+
 
 def card_by_id(request, card_id):
     if card_id > 10:
         return HttpResponse("Карточки нет!", status=404)
     return HttpResponse(f"Вы открыли карточку {card_id}")
+
 
 def get_all_cards(request):
     return HttpResponse("Все карточки")
