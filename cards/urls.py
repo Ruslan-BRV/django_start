daed8bd36586cdacdb90a47bc11dc841b7560f8b
Ruslan_BRV <ruslan10897@mail.ru>
2024-03-27 13:25:26 +0300

    Домашняя работа 32

--- cards/urls.py
+++ cards/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('categories/<slug:slug>/', views.get_cards_by_category, name='category'),  # Карточки по категории
     # path('tags/<slug:slug>/', views.get_cards_by_tag, name='tag'),  # Карточки по тегу
     path('<int:card_id>/detail/', views.get_detail_card_by_id, name='detail_card_by_id'),
-    path('tags/<int:tag_id>/', views.get_cards_by_tag, name='get_cards_by_tag'),
+    path('tags/<int:tag_id>/', views.get_cards_by_tag, name='cards_by_tag'),
 ]
